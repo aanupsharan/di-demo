c07e1cc3d47bd3279d151544f0702a7128cb6d38
aanupsharan <anup.ambula@gmail.com>
2020-01-26 21:17:11 -0600

    Accessing the controllers for the Application Context

---
+++ src/main/java/anup/springframework/didemo/controllers/MyController.java
@@ -0,0 +1,11 @@
+package anup.springframework.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String hello() {
+        System.out.println("Hello");
+        return "Hello";
+    }
+}
